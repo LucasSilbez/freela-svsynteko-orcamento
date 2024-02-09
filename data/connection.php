de19a31fcd13d5ee2ce4cf9d04ee3035ebfdf5fa
LucasSilbez <lucasdsilvab@gmail.com>
2024-02-09 07:08:10 -0300

    Feat: Cria sistema com PHP e MySQL

---
+++ data/connection.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost';
+$db = 'crud_synteko';
+$user = 'root';
+$pass = '';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Erro na conexão com o banco de dados: ' . $e->getMessage());
+}
+?>
